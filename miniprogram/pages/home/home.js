8bfc885cac84e757030a8c937eb911ebcd6b8e3c
love-to-yan <1757478118@qq.com>
2018-12-20 17:16:39 +0800

    first commit

---
+++ miniprogram/pages/home/home.js
@@ -0,0 +1,91 @@
+// pages/home/home.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    imgUrls: [
+
+      {
+
+        url: '/images/bg1.jpg'
+
+      }, {
+
+        url: '/images/bg2.jpg'
+
+      }, {
+
+        url: '/images/bg3.jpg'
+
+      }
+
+    ],
+
+    indicatorDots: true,  //小点
+
+    autoplay: true,  //是否自动轮播
+
+    interval: 3000,  //间隔时间
+
+    duration: 500,  //滑动时间
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
